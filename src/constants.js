97c2389a9ce542461beb6b437f30f1185dfea91c
BilalAhmadKhan2 <130067109+BilalAhmadKhan2@users.noreply.github.com>
2023-08-30 03:47:29 +0100

    Final Product

--- src/constants.js
+++ src/constants.js
@@ -1,3 +1,3 @@
 
-export const API_KEY = '';//Add your weatherapi.com key
+export const API_KEY = '5c33219d2f93467786e190538232607';//Add your weatherapi.com key
 export const API_URL = 'https://api.weatherapi.com/v1/forecast.json';
